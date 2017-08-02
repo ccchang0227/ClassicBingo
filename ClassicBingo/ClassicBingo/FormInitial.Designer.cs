35bc1306111379ffc004c86582f80f9ec3893401
Chih-chieh Chang <ccch.realtouch@gmail.com>
2017-08-02 23:57:41 +0800

    First Commit - 建立主畫面 - 完成基本按鈕動作

---
+++ ClassicBingo/ClassicBingo/FormInitial.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace ClassicBingo
+{
+    partial class FormInitial
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.tableLayout = new System.Windows.Forms.TableLayoutPanel();
+            this.buttonEnd = new System.Windows.Forms.Button();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.buttonRand = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.titleLabel.ForeColor = System.Drawing.SystemColors.Control;
+            this.titleLabel.Location = new System.Drawing.Point(12, 9);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(89, 21);
+            this.titleLabel.TabIndex = 1;
+            this.titleLabel.Text = "Initialize";
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // tableLayout
+            // 
+            this.tableLayout.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayout.ColumnCount = 5;
+            this.tableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayout.Location = new System.Drawing.Point(12, 62);
+            this.tableLayout.Name = "tableLayout";
+            this.tableLayout.RowCount = 5;
+            this.tableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayout.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayout.Size = new System.Drawing.Size(276, 276);
+            this.tableLayout.TabIndex = 2;
+            // 
+            // buttonEnd
+            // 
+            this.buttonEnd.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonEnd.AutoSize = true;
+            this.buttonEnd.CausesValidation = false;
+            this.buttonEnd.Font = new System.Drawing.Font("新細明體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.buttonEnd.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.buttonEnd.Location = new System.Drawing.Point(233, 9);
+            this.buttonEnd.Name = "buttonEnd";
+            this.buttonEnd.Size = new System.Drawing.Size(55, 26);
+            this.buttonEnd.TabIndex = 3;
+            this.buttonEnd.TabStop = false;
+            this.buttonEnd.Text = "結束";
+            this.buttonEnd.UseVisualStyleBackColor = true;
+            this.buttonEnd.Click += new System.EventHandler(this.ButtonEnd_Click);
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonReset.AutoSize = true;
+            this.buttonReset.CausesValidation = false;
+            this.buttonReset.Font = new System.Drawing.Font("新細明體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.buttonReset.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.buttonReset.Location = new System.Drawing.Point(172, 9);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(55, 26);
+            this.buttonReset.TabIndex = 4;
+            this.buttonReset.TabStop = false;
+            this.buttonReset.Text = "重置";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.ButtonReset_Click);
+            // 
+            // buttonRand
+            // 
+            this.buttonRand.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonRand.AutoSize = true;
+            this.buttonRand.CausesValidation = false;
+            this.buttonRand.Font = new System.Drawing.Font("新細明體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.buttonRand.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.buttonRand.Location = new System.Drawing.Point(111, 9);
+            this.buttonRand.Name = "buttonRand";
+            this.buttonRand.Size = new System.Drawing.Size(55, 26);
+            this.buttonRand.TabIndex = 5;
+            this.buttonRand.TabStop = false;
+            this.buttonRand.Text = "隨機";
+            this.buttonRand.UseVisualStyleBackColor = true;
+            this.buttonRand.Click += new System.EventHandler(this.ButtonRand_Click);
+            // 
+            // FormInitial
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(300, 350);
+            this.Controls.Add(this.buttonRand);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.buttonEnd);
+            this.Controls.Add(this.tableLayout);
+            this.Controls.Add(this.titleLabel);
+            this.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximumSize = new System.Drawing.Size(300, 350);
+            this.MinimumSize = new System.Drawing.Size(300, 350);
+            this.Name = "FormInitial";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Initialize";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.TableLayoutPanel tableLayout;
+        private System.Windows.Forms.Button buttonEnd;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.Button buttonRand;
+    }
+}
+
---
+++ ClassicBingo/ClassicBingo/FormInitial.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClassicBingo
+{
+    public partial class FormInitial : Form
+    {
+        public FormInitial()
+        {
+            InitializeComponent();
+
+            for (int i = 0; i < ButtonCnt; i++)
+            {
+                Button btn = new Button()
+                {
+                    Text = "",
+                    FlatStyle = FlatStyle.System,
+                    Parent = tableLayout,
+                    Dock = DockStyle.Fill,
+                    Tag = i + 1,
+                    Font = new Font("新細明體", 12, FontStyle.Bold),
+                    ForeColor = Color.Black,
+                    TabStop = false
+                };
+                btn.Click += OnButtonClick;
+
+                buttons.Add(btn);
+            }
+            
+            startInt = 1;
+
+        }
+
+        protected override void OnPaintBackground(PaintEventArgs e)
+        {
+            base.OnPaintBackground(e);
+
+            Rectangle rc = new Rectangle(1, 
+                1, 
+                this.Size.Width - 2, 
+                this.Size.Height - 2);
+            e.Graphics.DrawRectangle(Pens.Red, rc);
+        }
+
+        private static int ButtonCnt
+        {
+            get
+            {
+                return 25;
+            }
+        }
+        private List<Button> buttons = new List<Button>(capacity: ButtonCnt);
+        private int startInt;
+
+        private void OnButtonClick(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            btn.Text = "" + (startInt++);
+
+            btn.Enabled = false;
+        }
+
+        private void ButtonEnd_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void ButtonReset_Click(object sender, EventArgs e)
+        {
+            foreach (Button btn in buttons)
+            {
+                btn.Text = "";
+                btn.Enabled = true;
+            }
+        }
+
+        private void ButtonRand_Click(object sender, EventArgs e)
+        {
+            List<Button> cloneButtons = new List<Button>(buttons);
+            int start = 1;
+            Random random = new Random(Guid.NewGuid().GetHashCode());
+            for (int i = 0; i < ButtonCnt; i++)
+            {
+                if (cloneButtons.Count == 0)
+                {
+                    break;
+                }
+
+                int r = random.Next(cloneButtons.Count);
+                Button btn = cloneButtons.ElementAt(r);
+                btn.Text = "" + (start++);
+
+                btn.Enabled = false;
+                cloneButtons.Remove(btn);
+            }
+
+        }
+    }
+}
